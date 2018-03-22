96062669af91e0f6476dbcfaab31fb40bf1cbb09
Artem Molokov <molokov.artem@gmail.com>
2018-03-22 02:23:53 +0200

    Add webpack config and display ‘Hello, world’

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const config = {
+  entry: {
+    app: path.resolve(__dirname, "src", "index.js")
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+
+  devtool: "source-map",
+
+  watch: true,
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, "src"),
+      "node_modules"
+    ]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: "babel-loader",
+        exclude: /(node_modules)/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              modules: true,
+              localIdentName: "[local]--[hash:base64:5]"
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [
+                require("autoprefixer")()
+              ]
+            }
+          },
+          "sass-loader"
+        ],
+        include: [
+          /src/
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html"
+    })
+  ]
+};
+
+module.exports = config;
