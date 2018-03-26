3ecf68d30b88adecd7bbc84d06bedb706b6891bc
Artem Molokov <molokov.artem@gmail.com>
2018-03-27 02:54:12 +0300

    Add persistor to store

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,53 +2,56 @@ import React from "react";
 import { Provider } from "react-redux";
 import { Route, Switch, Redirect } from "react-router";
 import { ConnectedRouter } from "react-router-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import Layout from "components/Layout";
 import Navigate from "containers/Navigate";
 import Progress from "containers/Progress";
 import Stage from "containers/Stage";
 import Summary from "containers/Summary";
-import { store, history } from "store";
+import { store, history, persistor } from "store";
 import * as routes from "routes";
 
 const App = () => (
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Layout.Wrapper>
-        <Layout.Cover>
-          <Progress />
-        </Layout.Cover>
-        <Layout.Content>
-          <Switch>
-            <Route path={routes.SUMMARY} component={Summary} />
-            <Route path={routes.ANY_QUESTION} component={(route) => (
-              <Stage questionIndex={routes.getIndexFromRoute(route)}/>
-            )} />
-            <Route component={() => (
-              <Redirect to={{pathname: routes.FIRST_QUESTION}} />
-            )} />
-          </Switch>
-        </Layout.Content>
-        <Layout.Footer>
-          <Layout.Sidebar>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <Layout.Wrapper>
+          <Layout.Cover>
+            <Progress />
+          </Layout.Cover>
+          <Layout.Content>
             <Switch>
-              <Route path={routes.SUMMARY} component={() => (
-                <Navigate to={routes.LAST_QUESTION} label={"← Back to questions"} />
-              )} />
+              <Route path={routes.SUMMARY} component={Summary} />
               <Route path={routes.ANY_QUESTION} component={(route) => (
-                routes.isFirstQuestion(route) ? null : <Navigate to={routes.getPrevQuestion(route)} label={"← Back"} />
+                <Stage questionIndex={routes.getIndexFromRoute(route)}/>
+              )} />
+              <Route component={() => (
+                <Redirect to={{pathname: routes.FIRST_QUESTION}} />
               )} />
             </Switch>
-          </Layout.Sidebar>
-          <Layout.Sidebar>
-            <Route path={routes.ANY_QUESTION} component={(route) => (
-              routes.isLastQuestion(route) ?
-                <Navigate label={"See Summary"} to={routes.SUMMARY} requireAnswerFor={routes.getIndexFromRoute(route)} /> :
-                <Navigate label={"Next →"} to={routes.getNextQuestion(route)} requireAnswerFor={routes.getIndexFromRoute(route)} />
-            )} />
-          </Layout.Sidebar>
-        </Layout.Footer>
-      </Layout.Wrapper>
-    </ConnectedRouter >
+          </Layout.Content>
+          <Layout.Footer>
+            <Layout.Sidebar>
+              <Switch>
+                <Route path={routes.SUMMARY} component={() => (
+                  <Navigate to={routes.LAST_QUESTION} label={"← Back to questions"} />
+                )} />
+                <Route path={routes.ANY_QUESTION} component={(route) => (
+                  routes.isFirstQuestion(route) ? null : <Navigate to={routes.getPrevQuestion(route)} label={"← Back"} />
+                )} />
+              </Switch>
+            </Layout.Sidebar>
+            <Layout.Sidebar>
+              <Route path={routes.ANY_QUESTION} component={(route) => (
+                routes.isLastQuestion(route) ?
+                  <Navigate label={"See Summary"} to={routes.SUMMARY} requireAnswerFor={routes.getIndexFromRoute(route)} /> :
+                  <Navigate label={"Next →"} to={routes.getNextQuestion(route)} requireAnswerFor={routes.getIndexFromRoute(route)} />
+              )} />
+            </Layout.Sidebar>
+          </Layout.Footer>
+        </Layout.Wrapper>
+      </ConnectedRouter >
+    </PersistGate>
   </Provider>
 );
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,25 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { routerMiddleware } from "react-router-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import hardSet from "redux-persist/lib/stateReconciler/hardSet";
 import createHistory from "history/createHashHistory";
 import rootReducer from "reducers/root";
 
 const initialState = {};
 
+const persistedReducer = persistReducer({
+  key: "root",
+  storage,
+  blacklist: [],
+  stateReconciler: hardSet
+}, rootReducer);
+
 export const history = createHistory();
 
 export const store = createStore(
-  rootReducer,
+  persistedReducer,
   initialState,
   composeWithDevTools(
     applyMiddleware(
@@ -17,3 +27,5 @@ export const store = createStore(
     )
   )
 );
+
+export const persistor = persistStore(store);
